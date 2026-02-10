8f0809436c348f67e86e247d6d33765344d762fe
lucassmoro <lucas.sm.souza@gmail.com>
2026-02-10 15:11:04 -0300

    feat: vectornav launch file

---
+++ launch/amp_vectornav.launch.py
@@ -0,0 +1,38 @@
+import os
+import datetime
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration as LaunchConfig
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess , TimerAction, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument as LaunchArg
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    vectornav = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource([
+        os.path.join(
+            FindPackageShare('vectornav').find('vectornav'), 
+            'launch', 'vectornav.launch.py')
+        ])
+    )
+
+    pose = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join( 
+            get_package_share_directory('state_estimation'), 'launch'),
+            '/pose.launch.py'
+        ]),
+        launch_arguments={'input_pose':"/vectornav/pose",
+                          'output_pose':"/corrected_pose"}.items()
+    )
+
+
+   
+    return LaunchDescription([
+        vectornav,
+        pose
+    ])
