c5668b169f586d4e92675bc25d96af01a5bb0dcc
pmpossamai <pedrompossamai@gmail.com>
2026-04-10 19:38:36 -0300

    fix: changing nodes in amp_test_laserscan.launch for using sim time instead of actual time

--- launch/amp_test_laserscan.launch.py
+++ launch/amp_test_laserscan.launch.py
@@ -3,19 +3,28 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.substitutions import FindPackageShare
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true'
+    )
     
     laserscan = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('pointcloud_to_laserscan'),'launch'),
             '/amp_pointcloud_to_laserscan.launch.py'
             ]),
-            launch_arguments={'cloud_in':"/fsds/lidar/Lidar1",
-                              'scan':"/scan"}.items()
+            launch_arguments={'cloud_in':"/fsds/lidar/Lidar2",
+                              'scan':"/scan",
+                              'use_sim_time':use_sim_time}.items()
     )
     
     transform = IncludeLaunchDescription(
@@ -30,10 +39,21 @@ def generate_launch_description():
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('slam_toolbox'),'launch'),
             '/online_async_launch.py'
-            ])
-    ) 
+            ]),
+            launch_arguments={'use_sim_time':use_sim_time}.items()
+    )
+
+    ekf = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('robot_localization'),'launch'),
+            '/ekf_amp.launch.py'
+            ]),
+            launch_arguments={'use_sim_time':use_sim_time}.items()
+    )
 
     return LaunchDescription([
+        declare_use_sim_time_cmd,
+        ekf,
         laserscan,
         slam_toolbox
     ])
